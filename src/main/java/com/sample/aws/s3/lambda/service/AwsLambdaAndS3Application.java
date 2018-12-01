f018a857daf0287c19cd3a1a0e57868626d2136e
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-12-01 08:04:57 +0000

    init

---
+++ src/main/java/com/sample/aws/s3/lambda/service/AwsLambdaAndS3Application.java
@@ -0,0 +1,12 @@
+package com.sample.aws.s3.lambda.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaAndS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaAndS3Application.class, args);
+	}
+}
---
+++ src/test/java/com/sample/aws/s3/lambda/service/AwsLambdaAndS3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.aws.s3.lambda.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsLambdaAndS3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
